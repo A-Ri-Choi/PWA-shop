f0b28bf6d15147784c775c8d554406b97424f286
A-Ri-Choi <ari3253@naver.com>
2024-10-15 23:28:48 +0900

    first

---
+++ public/index.js
@@ -0,0 +1,11 @@
+// service worker Regigster 하기
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+      .register("./sw.js")
+      .then((sw) => {
+          console.log('서비스 워커가 등록됨!', sw);
+      })
+      .catch((err) => {
+          console.log('서비스 워커가 등록이 안됨!', err);
+      });
+}
