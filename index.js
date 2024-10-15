f53f82f64a383c70d5de18abdfbbb40095dd6452
A-Ri-Choi <ari3253@naver.com>
2024-10-16 00:13:30 +0900

    Fix sw

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // service worker Regigster 하기
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//       .register("/sw.js")
+//       .then((sw) => {
+//           console.log('서비스 워커가 등록됨!', sw);
+//       })
+//       .catch((err) => {
+//           console.log('서비스 워커가 등록이 안됨!', err);
+//       });
+// }
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-      .register("/sw.js")
-      .then((sw) => {
-          console.log('서비스 워커가 등록됨!', sw);
-      })
-      .catch((err) => {
-          console.log('서비스 워커가 등록이 안됨!', err);
-      });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
+    });
 }
