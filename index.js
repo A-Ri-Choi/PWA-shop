ac7afaac0a2dcc816d2143f4f7aca7cecc7330a7
A-Ri-Choi <ari3253@naver.com>
2024-10-16 00:37:48 +0900

    Fix error

--- index.js
+++ index.js
@@ -9,14 +9,3 @@
 //           console.log('서비스 워커가 등록이 안됨!', err);
 //       });
 // }
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-            console.error('Service Worker registration failed:', error);
-        });
-    });
-}
